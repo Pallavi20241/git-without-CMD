7538f0df9bbd5aa5d2e92fdd3743f948fb57fe09
Pallavi20241 <pikudjadhav@gmail.com>
2024-04-26 10:11:59 +0530

    commit

---
+++ src/witoutgitcmd1/PushToGithubWitoutCMD.java
@@ -0,0 +1,14 @@
+package witoutgitcmd1;
+
+public class PushToGithubWitoutCMD {
+
+	public static void main(String[] args) {
+		int a[]=new int[3];
+            a[0]=1;
+            a[1]=25;
+            a[2]=19;
+            
+            System.out.println(a[1]);
+	}
+
+}
